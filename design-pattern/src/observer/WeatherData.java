bcc6f3ac36f94814776d550301c34894fdce0528
thisisnew <thisisnew@naver.com>
2025-08-26 21:53:30 +0900

    챕터2: 옵저버 구현

---
+++ design-pattern/src/observer/WeatherData.java
@@ -0,0 +1,42 @@
+package observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<>();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observers.forEach(observer -> observer.update(temperature, humidity, pressure));
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
---
+++ design-pattern/src/observer/WeatherStation.java
@@ -0,0 +1,10 @@
+package observer;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+    }
+}
