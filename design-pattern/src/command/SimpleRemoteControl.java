2a794e8c44be1562c67c8ab23572ca8a94fa4c18
thisisnew <thisisnew@naver.com>
2025-09-02 18:50:01 +0900

    챕터6: 커맨드 패턴 구현

---
+++ design-pattern/src/command/SimpleRemoteControl.java
@@ -0,0 +1,14 @@
+package command;
+
+public class SimpleRemoteControl {
+    Command slot;
+    public SimpleRemoteControl() {}
+
+    public void setCommand(Command slot) {
+        this.slot = slot;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
--- design-pattern/src/factory/NyPizzaIngredientFactory.java
+++ design-pattern/src/factory/NyPizzaIngredientFactory.java
@@ -1,4 +1,33 @@
 package factory;
 
 public class NyPizzaIngredientFactory implements PizzaIngredientFactory{
+    @Override
+    public Dough createDough() {
+        return null;
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return null;
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return null;
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        return new Veggies[0];
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return null;
+    }
+
+    @Override
+    public Clams createClams() {
+        return null;
+    }
 }
